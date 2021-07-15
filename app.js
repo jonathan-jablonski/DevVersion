3537786d4e10568e9474f38fe3c5445f923b0e7c
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-07-14 19:36:11 -0500

    Building for heroku

--- app.js
+++ app.js
@@ -19,21 +19,19 @@ mongoose.connection.on("error", (err) => {
   console.log("error while connecting to DevVersionDB : ", err);
 });
 
-require("./models/user");
-require("./models/post");
+require("./server/models/user");
+require("./server/models/post");
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.use(require("./routes/auth"));
-app.use(require("./routes/post"));
-app.use(require("./routes/user"));
+app.use(require("./server/routes/auth"));
+app.use(require("./server/routes/post"));
+app.use(require("./server/routes/user"));
 
 // tells if application is on heroku
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static("client/build"));
 
 app.listen(PORT, () => {
   console.log(`Server is running under port ${PORT} ...`);
